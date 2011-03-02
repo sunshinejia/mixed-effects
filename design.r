5945e767ee586a2b92f0bdd850c94e170c63ebfa
Jia <proudlj@hotmail.com>
2011-03-02 16:11:11 -0600

    use F(x) to replace beta0

--- design.r
+++ design.r
@@ -929,6 +929,7 @@ sigma<-diag(2)
 sigma[1,1] <-1
 sigma[1,2] <-sigma[2,1]<-0.8
 sigma[2,2] <-1
+#Fx<-1-pnorm((2-2-2*0.05-2*0.1)/sqrt(1+0.05^2+0.1*0.8))
 set.seed(100)
 beta<- rmvnorm(15, mean, sigma)
 set.seed(100)
@@ -1300,19 +1301,20 @@ plot(c(stars.l,stars.r[-1]),c(yhat.l,yhat.r[-1]),main="20 points in total",xlab=
 abline(h=exp(-qchisq(0.95,1)/2))  
 lines(c(rev(stars.l),stars.r[-1]),c(rev(yhat.l),yhat.r[-1]))
 	
-#concentrate out the linear parameters
+#replace one of the linear parameters
+uf<-2
+x1<-0.05
+x2<-0.1
 full_loglike3<-function(ps){
     #browser()
-    beta<-c(ps[1],ps[2],ps[3])
-	error<-ps[6]
+    error<-ps[6]
 	k<-qnorm(1-ps[7])
-	if (k==0) k<-0.00001
-	const<-uf-beta%*%c(1,x1,x2)
-	const2<-const^2*ps[4]^2-error^2*k^2
-	if (const2<=0) res <- -1e15 else
-	{
-	a3<-(ps[4]*x1+ps[5])*error*k/sqrt(const2)
-	delta<-matrix(c(ps[4:5],0,a3),nrow=2,ncol=2,byrow=T)
+	delta<-matrix(c(ps[3:4],0,ps[5]),nrow=2,ncol=2,byrow=T)
+	sigma0<-error^2*(ps[4]^2+ps[5]^2)/(ps[3]*ps[5])
+	sigma1<-error^2/ps[5]^2
+	covar<-error^2*ps[4]^2/(ps[3]*ps[5]^2)
+	beta0<-uf-ps[1]*x1-ps[2]*x2-k*sqrt(sigma0+sigma1*x1^2+2*x1*covar)
+	beta<-c(beta0,ps[1],ps[2])
 	zz<-rbind(Zmat,delta)
 	zmat_qr<-qr(zz)
     Ri<-qr.R(zmat_qr,complete=TRUE)
@@ -1330,8 +1332,82 @@ full_loglike3<-function(ps){
 	tmp<-qr(tmp)
 	Rtmp<-qr.R(tmp,complete=TRUE)
     res<- -15*10*log(error^2) + 15*log(abs(det(delta)))-15*log(abs(det(R11)))-norm(Rtmp%*%c(-beta,1),"F")^2/(2*error^2)
-	
-	}
 	return(-res)
 }
+max_tran_linear<-nlminb(c(2,2,3,-1,1,1,0.7),full_loglike3,lower=c(rep(-Inf,5),0.001,0.001),upper=c(rep(Inf,6),0.999))
+est<-max_tran_linear$par
+delta<-matrix(c(est[3:4],0,est[5]),nrow=2,ncol=2,byrow=T)
+Dinv<-t(delta)%*%delta
+Sigmahat<-solve(Dinv)*est[6]^2
+sdev<-sqrt(diag(Sigmahat))
+tmp<-diag(1/sdev)
+corrm<- tmp%*%Sigmahat%*%tmp
+sigma0<-est[6]^2*(est[4]^2+est[5]^2)/(est[3]*est[5])
+sigma1<-est[6]^2/est[5]^2
+covar<-est[6]^2*est[4]^2/(est[3]*est[5]^2)
+beta0<-uf-est[1]*x1-est[2]*x2-qnorm(1-est[7])*sqrt(sigma0+sigma1*x1^2+2*x1*covar)
+#beta0<-uf-est[1]*x1-est[2]*x2-qnorm(1-est[7])*sqrt(sdev[1]^2+sdev[2]^2*x1^2+2*x1*corrm[1,2]*sdev[1]*sdev[2])
+
+prof_loglike3<-function(theta,ps){
+    #browser()
+	error<-ps[6]
+	k<-qnorm(1-theta)	
+	delta<-matrix(c(ps[3:4],0,ps[5]),nrow=2,ncol=2,byrow=T)
+	sigma0<-error^2*(ps[4]^2+ps[5]^2)/(ps[3]*ps[5])
+	sigma1<-error^2/ps[5]^2
+	covar<-error^2*ps[4]^2/(ps[3]*ps[5]^2)
+	beta0<-uf-ps[1]*x1-ps[2]*x2-k*sqrt(sigma0+sigma1*x1^2+2*x1*covar)
+	beta<-c(beta0,ps[1],ps[2])
+	zz<-rbind(Zmat,delta)
+	zmat_qr<-qr(zz)
+    Ri<-qr.R(zmat_qr,complete=TRUE)
+    Qi<-qr.Q(zmat_qr,complete=TRUE)
+    R11<-Ri[1:2,]
+    tmp<-matrix(0,nrow=15*20,ncol=4)
+	a<-list(NA);b<-NULL
+    for(i in 1:15){
+		a[[i]]<-t(Qi)%*%xx[[i]]
+		tmp[((i-1)*20+1):(i*20),1:3]<-a[[i]][3:22,]
+		b<-t(Qi)%*%yy[,i]
+		b<-as.vector(b)
+		tmp[((i-1)*20+1):(i*20),4]<-b[3:22]
+     }
+	tmp<-qr(tmp)
+	Rtmp<-qr.R(tmp,complete=TRUE)
+    result<- -15*10*log(error^2) + 15*log(abs(det(delta)))-15*log(abs(det(R11)))-norm(Rtmp%*%c(-beta,1),"F")^2/(2*error^2)
+	return(-result)
+}
+
+
+star<-max_tran_linear$par
+stars.l <- seq(from=star[7],to=0.001,length.out=10)
+opt_in<-nlminb(start=star[-7],prof_loglike3,theta=stars.l[2],lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
+yhat.l<-c(1,exp(-opt_in$objective+max_tran_linear$objective))
+pars<-opt_in$par
+for (st in stars.l[c(-1,-2)]){
+  res_opt<-nlminb(start=pars,prof_loglike3,theta=st,lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
+    obj <- res_opt$objective
+  yhat.l <-c(yhat.l,exp(-obj+max_tran_linear$objective))
+  pars <- res_opt$par
+}
+
+stars.r <- seq(from=star[7],to=0.999,length.out=11)
+#opt_in<-optim(star[-7],fn=prof_loglike2,theta=stars.r[2],lower=c(rep(0,4),-Inf,0.01),method="L-BFGS-B")
+opt_in<-nlminb(start=star[-7],prof_loglike3,theta=stars.r[2],lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
+yhat.r<-c(1,exp(-opt_in$objective+max_tran_linear$objective))
+pars<-opt_in$par
+lst <- list()
+lst <- c(opt_in)
+for (st in stars.r[c(-1,-2)]){
+  #res_opt<-optim(pars[-7],fn=prof_loglike2,theta=st,lower=c(rep(0,4),-Inf,0.01),method="L-BFGS-B")
+  res_opt<-nlminb(start=pars,prof_loglike3,theta=st,lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
+  lst <- c(lst,res_opt)
+  obj <- res_opt$objective
+  #if(exp(-res+max_tran2$objective)<0.5) browser()
+  yhat.r <-c(yhat.r,exp(-obj+max_tran_linear$objective))
+  pars <- res_opt$par
+}
+plot(c(stars.l,stars.r[-1]),c(yhat.l,yhat.r[-1]),main="20 points in total",xlab="F(x)",ylab="norm likelihood")
+abline(h=exp(-qchisq(0.95,1)/2))  
+lines(c(rev(stars.l),stars.r[-1]),c(rev(yhat.l),yhat.r[-1]))
 
