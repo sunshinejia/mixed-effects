84dc2408110c8ab5c4e13475961d6a83036179ac
Jia <proudlj@hotmail.com>
2011-04-06 12:00:07 -0500

    add simulation for POD

--- simulation.r
+++ simulation.r
@@ -6,19 +6,22 @@ library(Rsolnp)
 mean<-c(2,2)
 sigma<-diag(2)
 sigma[1,1] <-1
-sigma[1,2] <-sigma[2,1]<-0.8
+sigma[1,2] <-sigma[2,1]<-0.6
 sigma[2,2] <-1
 Fx<-1-pnorm((2-2-2*0.05-2*0.1)/sqrt(1+0.05^2+0.1*0.8))
 fix.beta<-2
-x1<- seq(0.05,by=0.05,length=20)
-x2<-seq(0.1,0.8,0.05)
+x1<- seq(from=-1,to=1,length=20)
+x2<-seq(from=-1,to=1,length=15)
 x1<-rep(x1,15)
 x2<-rep(x2,each=20)
 subject<-rep(seq(1,15,1),each=20)
-dt<-matrix(0,nrow=100,ncol=300);dataframe<-list(NA);result<-list(NA)
+dt<-matrix(0,nrow=n,ncol=300);dataframe<-list(NA);result<-list(NA)
+n<-1000
 i<-0
 j <- 1
-while (i <100){
+lst <- list()
+k <- 1
+while (i <n){
     set.seed(j)
     beta<- rmvnorm(15, mean, sigma)
     set.seed(j)
@@ -27,18 +30,22 @@ while (i <100){
     dat<-rand.beta[,1]+rand.beta[,2]*x1+fix.beta*x2+error
     data<-data.frame(cbind(subject,x1,x2,dat))
     data$subject<-as.factor(data$subject)
-    res<-try(lme(dat~x1+x2, data=data, random=~x1|subject,method="ML",control=list(maxIter=10000)),TRUE)
+    res<-try(lme(dat~x1+x2, data=data, random=~x1|subject,method="ML"),TRUE)
 	j <- j+1	
-	if (inherits(res, "try-error") != TRUE)
+	if (£¡inherits(res, "try-error") )
 	{
 	i<-i+1
 	dt[i,]<-dat
 	dataframe[[i]]<-data
 	result[[i]]<-res
+	}else{
+	lst[[k]] <- list(seed=j,data=data)
+	k <- k + 1
 	}
 }
+#j=2001 when -5 to 5; j=10008; j=2006 when -3 to 3 
 yy<-list(NA)
-for (j in 1:100){
+for (j in 1:n){
     yy[[j]]<-matrix(0,nrow=(20+2),ncol=15)
     for(i in 1:15){
 	     yy[[j]][1:20,i]<-dt[j,((i-1)*20+1): (i*20)]
@@ -90,7 +97,7 @@ full_loglike3<-function(y,ps){
 	return(-res)
 }
 max_tran_linear<-list(NA)
-for (i in 1:100) max_tran_linear[[i]]<-nlminb(c(2,2,3,-1,1,1,0.6),full_loglike3,y=yy[[i]],lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001,0.001),upper=c(rep(Inf,6),0.999))
+for (i in 1:n) max_tran_linear[[i]]<-nlminb(c(2,2,3,-1,1,1,0.6),full_loglike3,y=yy[[i]],lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001,0.001),upper=c(rep(Inf,6),0.999))
 
 est<-max_tran_linear$par
 delta<-matrix(c(est[3:4],0,est[5]),nrow=2,ncol=2,byrow=T)
@@ -137,18 +144,18 @@ prof_loglike3<-function(theta,y,ps){
 
 standard<-exp(-qchisq(0.95,1)/2)
 norm_value.l<-NULL;opt_in.l<-list(NA);norm_value.r<-NULL;opt_in.r<-list(NA);star<-matrix(NA,ncol=7,nrow=100)
-for (i in 1:100){
+for (i in 1:n){
     star[i,]<-max_tran_linear[[i]]$par
 	if (star[i,7]<=Fx){
         opt_in.r[[i]]<-nlminb(start=star[i,-7],prof_loglike3,y=yy[[i]],theta=Fx,lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
-	    norm_value.r[i]<-exp(-opt_in.r[[i]]$objective+max_tran_linear[[i]]$objective)
+	    norm_value.r <- c(norm_value.r,exp(-opt_in.r[[i]]$objective+max_tran_linear[[i]]$objective))
 	            } else {
 		opt_in.l[[i]]<-nlminb(start=star[i,-7],prof_loglike3,y=yy[[i]],theta=Fx,lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
-	    norm_value.l[i]<-exp(-opt_in.l[[i]]$objective+max_tran_linear[[i]]$objective)
+	    norm_value.l <- c(norm_value.l,exp(-opt_in.l[[i]]$objective+max_tran_linear[[i]]$objective))
                        }
 				} 
-num.l<-sum(norm_value.l<standard,na.rm=T) #the true value falls in the left tail; ans:1
-num.r<-sum(norm_value.r<standard,na.rm=T) #the true value falls in the right tail; ans:3
+num.l<-sum(norm_value.l<standard,na.rm=T) #the true value falls in the left tail; ans:0
+num.r<-sum(norm_value.r<standard,na.rm=T) #the true value falls in the right tail; ans:6
 			
 ####test to see whether the one-time optim and continuous optim agree
 stars.t<-matrix(NA,nrow=100,ncol=20);opt_test<-list(NA);yhat.t<-list(NA);res_test<-list(NA);pars.t<-matrix(NA,nrow=100,ncol=6)
@@ -167,3 +174,97 @@ for (i in 1:100){
  }
 yhat_true<-NULL
 for (i in 1:100) yhat_true[i]<-yhat.t[[i]][20]
+#################################################
+####test to see whether the one-time optim and two times optim agree
+stars.t<-matrix(NA,nrow=n,ncol=3);opt_test<-list(NA);yhat.t<-list(NA);res_test<-list(NA);pars.t<-matrix(NA,nrow=n,ncol=6)
+res_test<-list(NA);obj.t<-NULL
+for (i in 1:n){
+     stars.t[i,] <- seq(from=star[i,7],to=Fx,length.out=3)
+     opt_test[[i]]<-nlminb(start=star[i,-7],prof_loglike3,y=yy[[i]],theta=stars.t[i,2],lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
+	 pars.t[i,]<-opt_test[[i]]$par
+     res_test[[i]]<-nlminb(start=pars.t[i,],prof_loglike3,y=yy[[i]],theta=stars.t[i,3],lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
+     obj.t[i] <- res_test[[i]]$objective
+     yhat.t[[i]] <-exp(-obj.t[i]+max_tran_linear[[i]]$objective)
+   }
+
+#################################POD
+yth<-2
+x1<-0.05
+x2<-0.1
+full_loglike<-function(y,ps){
+    #browser()
+    error<-ps[6]
+	k<-qnorm(ps[7])
+	delta<-matrix(c(ps[3:4],0,ps[5]),nrow=2,ncol=2,byrow=T)
+	sigma0<-error^2*(ps[4]^2+ps[5]^2)/(ps[3]*ps[5])
+	sigma1<-error^2/ps[5]^2
+	rhosigma0<-error*ps[4]^2/(ps[3]^2*ps[5])
+	beta0<-k*sqrt(sigma0+sigma1*x1^2+2*x1*rhosigma0+error^2)+yth-ps[1]*x1-ps[2]*x2
+	beta<-c(beta0,ps[1],ps[2])
+	zz<-rbind(Zmat,delta)
+	zmat_qr<-qr(zz)
+    Ri<-qr.R(zmat_qr,complete=TRUE)
+    Qi<-qr.Q(zmat_qr,complete=TRUE)
+    R11<-Ri[1:2,]
+    tmp<-matrix(0,nrow=15*20,ncol=4)
+	a<-list(NA);b<-NULL
+    for(i in 1:15){
+		a[[i]]<-t(Qi)%*%xx[[i]]
+		tmp[((i-1)*20+1):(i*20),1:3]<-a[[i]][3:22,]
+		b<-t(Qi)%*%y[,i]
+		b<-as.vector(b)
+		tmp[((i-1)*20+1):(i*20),4]<-b[3:22]
+     }
+	tmp<-qr(tmp)
+	Rtmp<-qr.R(tmp,complete=TRUE)
+    res<- -15*10*log(error^2) + 15*log(abs(det(delta)))-15*log(abs(det(R11)))-norm(Rtmp%*%c(-beta,1),"F")^2/(2*error^2)
+	return(-res)
+}
+max_pod<-list(NA)
+for (i in 1:n) max_pod[[i]]<-nlminb(c(2,2,3,-1,1,1,0.6),full_loglike,y=yy[[i]],lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001,0.001),upper=c(rep(Inf,6),0.999))
+
+prof_loglike<-function(theta,y,ps){
+    #browser()
+	error<-ps[6]
+	k<-qnorm(theta)	
+	delta<-matrix(c(ps[3:4],0,ps[5]),nrow=2,ncol=2,byrow=T)
+	sigma0<-error^2*(ps[4]^2+ps[5]^2)/(ps[3]*ps[5])
+	sigma1<-error^2/ps[5]^2
+	rhosigma0<-error*ps[4]^2/(ps[3]^2*ps[5])
+	beta0<-k*sqrt(sigma0+sigma1*x1^2+2*x1*rhosigma0+error^2)+yth-ps[1]*x1-ps[2]*x2
+	beta<-c(beta0,ps[1],ps[2])
+	zz<-rbind(Zmat,delta)
+	zmat_qr<-qr(zz)
+    Ri<-qr.R(zmat_qr,complete=TRUE)
+    Qi<-qr.Q(zmat_qr,complete=TRUE)
+    R11<-Ri[1:2,]
+    tmp<-matrix(0,nrow=15*20,ncol=4)
+	a<-list(NA);b<-NULL
+    for(i in 1:15){
+		a[[i]]<-t(Qi)%*%xx[[i]]
+		tmp[((i-1)*20+1):(i*20),1:3]<-a[[i]][3:22,]
+		b<-t(Qi)%*%y[,i]
+		b<-as.vector(b)
+		tmp[((i-1)*20+1):(i*20),4]<-b[3:22]
+     }
+	tmp<-qr(tmp)
+	Rtmp<-qr.R(tmp,complete=TRUE)
+    result<- -15*10*log(error^2) + 15*log(abs(det(delta)))-15*log(abs(det(R11)))-norm(Rtmp%*%c(-beta,1),"F")^2/(2*error^2)
+	return(-result)
+}
+
+stan_pod<-exp(-qchisq(0.95,1)/2)
+norm_pod.l<-NULL;opt_pod.l<-list(NA);norm_pod.r<-NULL;opt_pod.r<-list(NA);start_pod<-matrix(NA,ncol=7,nrow=n)
+for (i in 1:n){
+    start_pod[i,]<-max_pod[[i]]$par
+	if (start_pod[i,7]<=POD){
+        opt_pod.r[[i]]<-nlminb(start=start_pod[i,-7],prof_loglike,y=yy[[i]],theta=POD,lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
+	    norm_pod.r <- c(norm_pod.r,exp(-opt_pod.r[[i]]$objective+max_pod[[i]]$objective))
+	            } else {
+		opt_pod.l[[i]]<-nlminb(start=start_pod[i,-7],prof_loglike,y=yy[[i]],theta=POD,lower=c(rep(-Inf,2),0.001,-Inf,0.001,0.001))
+	    norm_pod.l <- c(norm_pod.l,exp(-opt_pod.l[[i]]$objective+max_pod[[i]]$objective))
+                       }
+				} 
+num_pod.l<-sum(norm_pod.l<stan_pod) #the true value falls in the left tail; ans:0
+num_pod.r<-sum(norm_pod.r<stan_pod) #the true value falls in the right tail; ans:6
+			
